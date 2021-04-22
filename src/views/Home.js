62110bc0cff85179a8cda4d19fa9b2c9684772ea
Curtis M <34104460+CurtisMac@users.noreply.github.com>
2021-04-22 23:16:05 +0100

    builds out Home component

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,11 +1,36 @@
-import React from 'react';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
 
-function Upload(props) {
+//components
+import Card from "../components/home/Card";
+
+//styles
+const useStyles = makeStyles((theme) => ({
+    grid: {
+        display: "grid",
+        gridTemplateColumns: "1fr",
+        gridAutoFlow: "dense",
+        gap: "10px 0",
+        [theme.breakpoints.up("md")]: {
+            gridTemplateColumns: "repeat(2, 1fr)",
+            gap: "10px",
+        },
+        [theme.breakpoints.up("lg")]: {
+            gridTemplateColumns: "repeat(4, 1fr)",
+            gap: "10px",
+        },
+    },
+}));
+
+function Home({ images, dispatch }) {
+    const classes = useStyles();
     return (
-        <div>
-            View some cats
+        <div className={classes.grid}>
+            {images.map((img) => (
+                <Card key={img.id} dispatch={dispatch} img={img} />
+            ))}
         </div>
     );
 }
 
-export default Upload;
+export default Home;
