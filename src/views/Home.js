ef80871985a2f7bee71b837ff563d83ea50e446b
Curtis M <34104460+CurtisMac@users.noreply.github.com>
2021-04-23 08:46:03 +0100

    renames Card component

--- src/views/Home.js
+++ src/views/Home.js
@@ -2,7 +2,7 @@ import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 
 //components
-import Card from "../components/home/Card";
+import ImageCard from "../components/home/ImageCard";
 
 //styles
 const useStyles = makeStyles((theme) => ({
@@ -27,7 +27,7 @@ function Home({ images, dispatch }) {
     return (
         <div className={classes.grid}>
             {images.map((img) => (
-                <Card key={img.id} dispatch={dispatch} img={img} />
+                <ImageCard key={img.id} dispatch={dispatch} img={img} />
             ))}
         </div>
     );
