e77fd9bf746c50f75a34c5bf44827d6f13edab1e
Curtis M <34104460+CurtisMac@users.noreply.github.com>
2021-04-24 16:07:38 +0100

    adds Alert component

---
+++ src/components/Alert.js
@@ -0,0 +1,44 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Snackbar from "@material-ui/core/Snackbar";
+import MuiAlert from "@material-ui/lab/Alert";
+
+function Alert({ msg, type, setAlert }) {
+    const handleClose = () => {
+        setAlert({ msg: "", type: "" });
+    };
+    return (
+        <div>
+            <Snackbar
+                open={msg.length ? true : false}
+                autoHideDuration={6000}
+                onClose={handleClose}
+                anchorOrigin={{
+                    vertical: "bottom",
+                    horizontal: "center",
+                }}
+            >
+                <MuiAlert
+                    onClose={handleClose}
+                    severity={type}
+                    elevation={6}
+                    variant="filled"
+                >
+                    {msg}
+                </MuiAlert>
+            </Snackbar>
+        </div>
+    );
+}
+
+export default Alert;
+
+Alert.defaultProps = {
+    type: "info"
+}
+
+Alert.propTypes = {
+    msg: PropTypes.string.isRequired,
+    type: PropTypes.oneOf(["error", "success", "warning", "info"]),
+    setAlert: PropTypes.func.isRequired,
+};
