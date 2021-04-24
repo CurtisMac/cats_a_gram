1125a39f8763814e59160cd50458874ea0cb47a9
Curtis M <34104460+CurtisMac@users.noreply.github.com>
2021-04-24 21:38:46 +0100

    better scoping in imagesReducer.js

--- src/utils/imagesReducer.js
+++ src/utils/imagesReducer.js
@@ -1,19 +1,20 @@
 import update from "immutability-helper";
 
 function reducer(state, action) {
-    const { img, favId } = action.payload;
-    const i = state.findIndex((obj) => obj.id === img);
-    switch (action.type) {
-        case "set":
-            return action.payload;
-        case "delete":
-            return update(state, {$splice: [[i, 1]] });
-        case "toggleFavourite":
-            return update(state, {
-                [i]: { $merge: { favourite: favId } },
-            });
-        default:
-            throw new Error();
+    if (action.type === "set") {
+        return action.payload;
+    }
+    if (action.type === "delete") {
+        const { img } = action.payload;
+        const i = state.findIndex((obj) => obj.id === img);
+        return update(state, {$splice: [[i, 1]] });
+    }
+    if (action.type === "toggleFavourite") {
+        const { img, favId } = action.payload;
+        const i = state.findIndex((obj) => obj.id === img);
+        return update(state, {
+            [i]: { $merge: { favourite: favId } },
+        });
     }
 }
 
