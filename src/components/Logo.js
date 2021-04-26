26a019cf0219aafe2f2d8aa6610c04e1d5d996ff
Curtis M <34104460+CurtisMac@users.noreply.github.com>
2021-04-26 01:01:55 +0100

    logo resizes to width

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,11 +1,21 @@
 import React from "react";
 import logo from "../assets/logo.svg";
 import { Link } from "react-router-dom";
+import { makeStyles } from "@material-ui/core/styles";
+
+//styles
+const useStyles = makeStyles(() => ({
+    img: {
+        width: "100%",
+        paddingRight: "20px"
+    },
+}));
 
 function Logo() {
+    const classes = useStyles();
     return (
-        <Link to="/">
-            <img src={logo} alt="Logo" />
+        <Link to="/" >
+            <img src={logo} alt="Logo" className={classes.img} />
         </Link>
     );
 }
